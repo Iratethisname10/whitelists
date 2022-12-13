bbc54a2e4619cd22ed5a2d586a0fe42c70ed61d4
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-12-13 18:30:02 +0800

    Add files via upload

--- tags/tagloader.lua
+++ tags/tagloader.lua
@@ -1,8 +1,10 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/whitelists/main/whitelist3.lua"))()
+
 if getgenv().TagVisible == true then
     if _G.isPrivet == true then
-
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/whitelists/main/tags/privet.lua"))()
     end
     _G.isOwner == true then
-        
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/whitelists/main/tags/owner.lua"))()
     end
 end
